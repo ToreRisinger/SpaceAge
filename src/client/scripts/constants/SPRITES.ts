785cc7a9fbe4ef2471ed694afacba9d73a87affa
ToreRisinger <tore.risinger@gmail.com>
2019-03-11 22:56:36 +0100

    Added a bunch of dummy module sprites

--- src/client/scripts/constants/SPRITES.ts
+++ src/client/scripts/constants/SPRITES.ts
@@ -32,6 +32,83 @@ export const SPRITES = {
         anims: { }
     },
 
+    REPAIR_FACILITY_MODULE_I: {
+        key: "repair_facility_module_I",
+        file: "modules/repair_facility_module_I.png",
+        width: 38,
+        height: 38,
+        anims: {
+            ANIM_1: {
+                key: "repair_facility_module_I",
+                frameRate: 4,
+                repeat: -1, //forever
+                frames: [0, 1, 2, 3, 4, 5, 6]
+            }
+        }
+    },
+
+    SHIELD_GENERATOR_MODULE_I: {
+        key: "shield_generator_module_I",
+        file: "modules/shield_generator_module_I.png",
+        width: 38,
+        height: 38,
+        anims: {
+            ANIM_1: {
+                key: "shield_generator_module_I",
+                frameRate: 4,
+                repeat: -1, //forever
+                frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+            }
+        }
+    },
+
+    TARGETING_SYSTEM_MODULE_I: {
+        key: "targeting_system_module_I",
+        file: "modules/targeting_system_module_I.png",
+        width: 38,
+        height: 38,
+        anims: {
+            ANIM_1: {
+                key: "targeting_system_module_I",
+                frameRate: 4,
+                repeat: -1, //forever
+                frames: [0, 1, 2, 3, 4, 5, 6, 7]
+            }
+        }
+    },
+
+    AVOIDANCE_SYSTEM_MODULE_I: {
+        key: "avoidance_system_module_I",
+        file: "modules/avoidance_system_module_I.png",
+        width: 38,
+        height: 38,
+        anims: { }
+    },
+
+    POWER_GENERATOR_MODULE_I: {
+        key: "power_generator_module_I",
+        file: "modules/power_generator_module_I.png",
+        width: 38,
+        height: 38,
+        anims: { }
+    },
+
+    
+    RADAR_MODULE_I: {
+        key: "radar_module_I",
+        file: "modules/radar_module_I.png",
+        width: 38,
+        height: 38,
+        anims: {
+            ANIM_1: {
+                key: "radar_module_I",
+                frameRate: 4,
+                repeat: -1, //forever
+                frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+            } 
+        }
+    },
+
     ENGINE_MODULE_I: {
         key: "engine_module_I",
         file: "modules/engine_module_I.png",
